b3320e7fa91193d5db5cea2a58d7830bcb3f8d23
Yago Cunha <yago.cunha123@gmail.com>
2025-05-20 10:51:52 -0300

    Add API endpoint to list todos and implement frontend functionality to fetch and create todos. Update form component to handle user input and display todos dynamically.

--- apps/backend/app/api/todo.py
+++ apps/backend/app/api/todo.py
@@ -3,6 +3,7 @@ from sqlmodel import Session
 from app.schemas.todo import TodoResponse, TodoBase
 from app.core.database import get_session
 from app.services.todo import TodoService
+from typing import List
 
 router = APIRouter()
 
@@ -16,4 +17,13 @@ def create_todo(
         todo = todo_service.create_todo(todo_create)
         return todo
     except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@router.get("/todos", response_model=List[TodoResponse])
+def list_todos(*,  session: Session = Depends(get_session)) -> List[TodoResponse]:
+    try:
+        todo_service = TodoService(session)
+        todos = todo_service.list_todos()
+        return todos
+    except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
--- apps/frontend/src/form.tsx
+++ apps/frontend/src/form.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import { useEffect, useState } from "react"
 
 interface Todo {
   id: number
@@ -7,12 +8,12 @@ interface Todo {
   done: boolean
 }
 
-const todos: Todo[] = [
-  { id: 1, description: "Item 1", done: false },
-  { id: 2, description: "Item 2", done: false },
-  { id: 3, description: "Item 3", done: false },
-  { id: 4, description: "Item 4", done: true },
-]
+// const todos: Todo[] = [
+//   { id: 1, description: "Item 1", done: false },
+//   { id: 2, description: "Item 2", done: false },
+//   { id: 3, description: "Item 3", done: false },
+//   { id: 4, description: "Item 4", done: true },
+// ]
 
 function TodoItem({ description }: { description: string }) {
   return (
@@ -23,12 +24,42 @@ function TodoItem({ description }: { description: string }) {
   )
 }
 
+async function fetchTodos(): Promise<Todo[]> {
+  const todoList = await fetch("http://localhost:8000/api/todos").then(response => response.json())
+  return todoList
+}
+
+async function createTodo(description: string): Promise<void> {
+  fetch("http://localhost:8000/api/todos", {
+    method: "POST", body: JSON.stringify({ description }), headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(response => console.log(response))
+}
+
 export function Form() {
+  const [todos, setTodos] = useState<Todo[]>([])
+  const [inputValue, setInputValue] = useState("")
+
+  function refreshTodos() {
+    fetchTodos().then(response => setTodos(response))
+  }
+
+  useEffect(() => {
+    refreshTodos()
+  }, [])
+
+  const handleCreateTodo = async () => {
+    await createTodo(inputValue)
+    setInputValue("")
+    refreshTodos()
+  }
+
   return (
     <div className="px-120 py-10">
       <div className="flex gap-2">
-        <Input type="text" placeholder="Task Ex: Buy Shampoo" />
-        <Button>Add</Button>
+        <Input type="text" placeholder="Task Ex: Buy Shampoo" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+        <Button onClick={handleCreateTodo}>Add</Button>
       </div>
       <div className="flex justify-between mt-4">
         <div>
