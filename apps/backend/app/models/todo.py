2dc5b7e9ee0efb43f3e748ee43861eae461c3740
Yago Cunha <yago.cunha123@gmail.com>
2025-05-20 09:40:24 -0300

    Set default value for 'done' field in Todo model to False

--- apps/backend/app/models/todo.py
+++ apps/backend/app/models/todo.py
@@ -5,4 +5,4 @@ from typing import Optional
 class Todo(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     description: str
-    done: bool
+    done: bool = Field(default=False)
